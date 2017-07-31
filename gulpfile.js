42245d261d4da9c38d6b56e5b52d37cb95826389
igormaruni4 <igormaruni4@gmail.com>
2017-07-31 12:24:37 +0300

    deleted_node_modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,47 @@
 
-var gulp = require('gulp');
-var stylus = require('gulp-stylus');
-var sourcemaps = require('gulp-sourcemaps');
+const gulp = require('gulp');
+const stylus = require('gulp-stylus');
+const sourcemaps = require('gulp-sourcemaps');
+const csso = require('gulp-csso');
+const concatCss = require('gulp-concat-css');
+const compiler = require('google-closure-compiler-js').gulp();
+
+/*********************************************
+ * Объединение и минимизация CSS
+ ***********************************************/
+
+gulp.task('concat', function () {
+  return gulp.src('css/*.css')
+    .pipe(concatCss("main.min.css"))
+    .pipe(gulp.dest('css/concat/'));
+});
+
+gulp.task('csso', function () {
+  return gulp.src('css/concat/main.min.css')
+    .pipe(csso())
+    .pipe(gulp.dest('public'));
+});
+
+/*********************************************
+ * Объединение и минимизация JS
+ ***********************************************/
+
+gulp.task('goog', function() {
+  // select your JS code here
+  return gulp.src('js/*.js', {base: './'})
+    .pipe(compiler({
+      compilation_level: 'SIMPLE',
+      warning_level: 'DEFAULT',
+      output_wrapper: '(function(){\n%output%\n}).call(this)',
+      js_output_file: 'main.min.js',  // outputs single file
+      create_source_map: true
+    }))
+    .pipe(gulp.dest('public'));
+});
+
+/*********************************************
+ * Наблюдения за изменениями файлов
+ ***********************************************/
 
 gulp.task('styl', function () {
   return gulp.src('stylus/*.styl')
